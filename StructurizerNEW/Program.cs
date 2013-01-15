04130ff8fc24c957bf4376b83364e0b2d0ee8ef5
Bram De Moor <bram.demoor@gmail.com>
2013-01-15 21:27:24 +0100

    Simple UI

--- StructurizerNEW/Program.cs
+++ StructurizerNEW/Program.cs
@@ -2,87 +2,32 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 using Newtonsoft.Json;
 using StructurizerNEW.Domain;
 using StructurizerNEW.Extra;
 
 namespace StructurizerNEW
 {
-    /// <summary>
-    /// The program is responsible for managing different content roots.
-    /// </summary>
     class Program
-    {
-        private static readonly List<FolderEntity> activeRoots = new List<FolderEntity>();
-
+    {        
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("StructurizR ||| Simple, convention-based document processor");            
-            Console.WriteLine();
+            var configFilePath = args.FirstOrDefault();
 
-            if (!args.Any())
+            if(configFilePath != null)
             {
-                Console.WriteLine("Please provide a config file as parameter");
-                Console.ReadLine();
-                return;
+                if (!File.Exists(configFilePath))
+                {
+                    Console.WriteLine("Specified config file not found: {0}", configFilePath);
+                    Console.ReadLine();
+                    return;
+                }
             }
 
-            var configFilePath = args.First();
-
-            if (!File.Exists(configFilePath))
-            {
-                Console.WriteLine("Specified config file not found: {0}", configFilePath);
-                Console.ReadLine();
-                return;
-            }
-
-            Console.WriteLine("Loading {0}...", configFilePath);
-
-            using (var sr = new StreamReader(configFilePath))
-            {
-                var configObj = JsonConvert.DeserializeObject<CoreFile>(sr.ReadToEnd());
-
-                FolderEntity newobj = null;
-
-                foreach (var root in configObj.Roots)
-                {
-                    switch (root.Type)
-                    {
-                        case "Project":
-                            newobj = new Project(new DirectoryInfo(root.Path));
-                            break;                        
-                        default:
-                            Console.WriteLine("Unsupported type");
-                            break;
-                    }
-
-                    if (root.WatchFiles)
-                    {
-                        new FileWatcher(newobj);
-                    }   
-
-                    activeRoots.Add(newobj);
-                }
-            }        
-
-            var loop = true;
-
-            do
-            {
-                switch (Console.ReadLine())
-                {
-                    case "exit":
-                        loop = false;
-                        break;
-                    case "build":
-                        foreach (var processingRoot in activeRoots)
-                        {
-                            processingRoot.Process();
-                        }
-                        break;
-                }
-            } 
-            while (loop);
+            Application.EnableVisualStyles();
+            Application.Run(new frmMain(configFilePath));                              
         }     
     }
 }
---
+++ StructurizerNEW/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using StructurizerNEW.Domain;
+using StructurizerNEW.Extra;
+
+namespace StructurizerNEW
+{
+    public partial class frmMain : Form
+    {
+        private readonly List<FolderEntity> activeRoots = new List<FolderEntity>();
+
+        public frmMain(string configPath = null)
+        {
+            InitializeComponent();
+
+            LoadConfig(configPath);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picGitHub_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/bramdemoor/Structurizr");
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            foreach (var processingRoot in activeRoots)
+            {
+                processingRoot.Process();
+            }
+        }
+
+        private void LoadConfig(string configFilePath)
+        {
+            treeView1.Nodes.Clear();
+            var applicationNode = treeView1.Nodes.Add("Structurizr");
+
+            using (var sr = new StreamReader(configFilePath))
+            {
+                var configObj = JsonConvert.DeserializeObject<CoreFile>(sr.ReadToEnd());
+
+                FolderEntity newobj = null;
+
+                foreach (var root in configObj.Roots)
+                {
+                    switch (root.Type)
+                    {
+                        case "Project":
+                            newobj = new Project(new DirectoryInfo(root.Path));
+                            applicationNode.Nodes.Add(root.Path);                                
+                            break;
+                    }
+
+                    if (root.WatchFiles)
+                    {
+                        new FileWatcher(newobj);
+                    }
+
+                    activeRoots.Add(newobj);
+                }
+            }
+
+            applicationNode.ExpandAll();
+        }
+    }
+}
